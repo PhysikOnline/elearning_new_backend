649492958691bfa1edf71210a61b2bd112a23f0e
Bomel <keiwanjamaly@protonmail.com>
2019-10-19 13:45:15 +0200

    Changed database

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,16 @@
-DROP TABLE IF EXISTS `CoursePermissions`;
-DROP TABLE IF EXISTS `Course`;
-DROP TABLE IF EXISTS `Semester`;
-DROP TABLE IF EXISTS `User`;
-DROP TABLE IF EXISTS `Sessions`;
+DROP TABLE IF EXISTS `GroupUser`;
 
+DROP TABLE IF EXISTS `CoursePermissions`;
+
+DROP TABLE IF EXISTS `Groups`;
+
+DROP TABLE IF EXISTS `Course`;
+
+DROP TABLE IF EXISTS `Semester`;
+
+DROP TABLE IF EXISTS `User`;
+
+DROP TABLE IF EXISTS `Sessions`;
 
 CREATE TABLE `User` (
   `Login` char(8) NOT NULL,
@@ -22,8 +29,17 @@ CREATE TABLE `Course` (
   `Name` varchar(60) NOT NULL,
   `Semester` varchar(10) NOT NULL,
   `Description` LONGTEXT NULL,
+  `GroupVisible` BOOLEAN NOT NULL default 0,
+  `GroupTimerActive` BOOLEAN NOT NULL default 0,
+  `GroupTimer` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Name`, `Semester`),
-  FOREIGN KEY (`Semester`) REFERENCES `Semester`(`Semester`)
+  FOREIGN KEY (`Semester`) REFERENCES `Semester`(`Semester`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CHECK (
+    NOT (
+      `GroupVisible`
+      AND `GroupTimerActive`
+    )
+  )
 );
 
 CREATE TABLE `CoursePermissions` (
@@ -32,9 +48,26 @@ CREATE TABLE `CoursePermissions` (
   `Login` char(8) NOT NULL,
   `Permissions` varchar(6),
   PRIMARY KEY (`Name`, `Semester`, `Login`),
-  FOREIGN KEY (`Name`, `Semester`) REFERENCES `Course`(`Name`, `Semester`) ,
-  FOREIGN KEY (`Login`) REFERENCES `User`(`Login`),
+  FOREIGN KEY (`Name`, `Semester`) REFERENCES `Course`(`Name`, `Semester`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`Login`) REFERENCES `User`(`Login`) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK (`Permissions` IN ('user', 'admin'))
 );
 
+CREATE TABLE `Groups` (
+  `CourseName` varchar(60) NOT NULL,
+  `Semester` varchar(10) NOT NULL,
+  `GroupName` varchar(20) NULL,
+  `Ordering` INT NOT NULL UNIQUE AUTO_INCREMENT,
+  PRIMARY KEY (`CourseName`, `Semester`, `GroupName`),
+  FOREIGN KEY (`CourseName`, `Semester`) REFERENCES `Course`(`Name`, `Semester`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
+CREATE TABLE `GroupUser` (
+  `CourseName` varchar(60) NOT NULL,
+  `Semester` varchar(10) NOT NULL,
+  `GroupName` varchar(20) NOT NULL,
+  `Login` char(8) NOT NULL,
+  PRIMARY KEY (`CourseName`, `Semester`, `Login`),
+  FOREIGN KEY (`Login`) REFERENCES `User`(`Login`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`CourseName`, `Semester`, `GroupName`) REFERENCES `Groups`(`CourseName`, `Semester`, `GroupName`) ON DELETE CASCADE ON UPDATE CASCADE
+);
--- db/testdata.sql
+++ db/testdata.sql
@@ -1,37 +1,133 @@
-INSERT INTO `User`
-(`Login`, `password`)
+INSERT INTO
+    `User` (`Login`, `password`)
 VALUES
-("s0000000", PASSWORD("s0000000")), ("s0000001", PASSWORD("s0000001")), ("s0000002", PASSWORD("s0000002"));
+    ("s0000000", PASSWORD("s0000000")),
+    ("s0000001", PASSWORD("s0000001")),
+    ("s0000002", PASSWORD("s0000002"));
 
-
-INSERT INTO `Semester`
-(`Semester`, `Ordering`)
+INSERT INTO
+    `Semester` (`Semester`, `Ordering`)
 VALUES
-("SoSe 19", 1),("WiSe 19/20", 2);
+    ("SoSe 19", 1),("WiSe 19/20", 2);
 
-
-INSERT INTO `Course`
-(`Name`,
-`Semester`, `Description`)
+INSERT INTO
+    `Course` (`Name`, `Semester`, `Description`)
 VALUES
-("Grundlagen in Mobbing 2", "WiSe 19/20", "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"),
-("Opferdarstellung 1", "SoSe 19", "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"),
-("Grundlagen in Mobbing 1", "SoSe 19", "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"),
-("Opferdarstellung 2", "WiSe 19/20", "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"),
-("Psychologie der erfolgreichen Erpressung 1", "SoSe 19", "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"),
-("Gewaltsamer Überfall für Fortgeschrittene", "WiSe 19/20", "<p>Hier sollte eine schöne beschreibung stehen!!!</p>");
+    (
+        "Opferdarstellung 1",
+        "SoSe 19",
+        "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"
+    ),
+    (
+        "Grundlagen in Mobbing 1",
+        "SoSe 19",
+        "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"
+    ),
+    (
+        "Opferdarstellung 2",
+        "WiSe 19/20",
+        "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"
+    ),
+    (
+        "Psychologie der erfolgreichen Erpressung 1",
+        "SoSe 19",
+        "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"
+    ),
+    (
+        "Gewaltsamer Überfall für Fortgeschrittene",
+        "WiSe 19/20",
+        "<p>Hier sollte eine schöne beschreibung stehen!!!</p>"
+    );
 
-INSERT INTO `CoursePermissions`
-(`Name`,
-`Semester`, 
-`Login`, 
-`Permissions`)
+INSERT INTO
+    `Course` (
+        `Name`,
+        `Semester`,
+        `Description`,
+        `GroupVisible`,
+        `GroupTimerActive`
+    )
 VALUES
-("Grundlagen in Mobbing 2", "WiSe 19/20", "s0000000", "admin"),
-("Grundlagen in Mobbing 2", "WiSe 19/20", "s0000002", "user"),
-("Opferdarstellung 1", "SoSe 19", "s0000000", "admin"),
-("Grundlagen in Mobbing 1", "SoSe 19", "s0000000", "admin"),
-("Opferdarstellung 2", "WiSe 19/20", "s0000000", "admin"),
-("Psychologie der erfolgreichen Erpressung 1", "SoSe 19", "s0000000", "admin"),
-("Psychologie der erfolgreichen Erpressung 1", "SoSe 19", "s0000002", "user"),
-("Gewaltsamer Überfall für Fortgeschrittene", "WiSe 19/20", "s0000000", "admin");
+    (
+        "Grundlagen in Mobbing 2",
+        "WiSe 19/20",
+        "<p>Hier sollte eine schöne beschreibung stehen!!!</p>",
+        1,
+        0
+    );
+
+INSERT INTO
+    `CoursePermissions` (`Name`, `Semester`, `Login`, `Permissions`)
+VALUES
+    (
+        "Grundlagen in Mobbing 2",
+        "WiSe 19/20",
+        "s0000000",
+        "admin"
+    ),
+    (
+        "Grundlagen in Mobbing 2",
+        "WiSe 19/20",
+        "s0000002",
+        "user"
+    ),
+    (
+        "Opferdarstellung 1",
+        "SoSe 19",
+        "s0000000",
+        "admin"
+    ),
+    (
+        "Grundlagen in Mobbing 1",
+        "SoSe 19",
+        "s0000000",
+        "admin"
+    ),
+    (
+        "Opferdarstellung 2",
+        "WiSe 19/20",
+        "s0000000",
+        "admin"
+    ),
+    (
+        "Psychologie der erfolgreichen Erpressung 1",
+        "SoSe 19",
+        "s0000000",
+        "admin"
+    ),
+    (
+        "Psychologie der erfolgreichen Erpressung 1",
+        "SoSe 19",
+        "s0000002",
+        "user"
+    ),
+    (
+        "Gewaltsamer Überfall für Fortgeschrittene",
+        "WiSe 19/20",
+        "s0000000",
+        "admin"
+    );
+
+INSERT INTO
+    `Groups` (`CourseName`, `Semester`, `GroupName`)
+VALUES
+    (
+        "Grundlagen in Mobbing 2",
+        "WiSe 19/20",
+        "Gruppe 1"
+    ),
+    (
+        "Grundlagen in Mobbing 2",
+        "WiSe 19/20",
+        "Gruppe 2"
+    ),
+    (
+        "Grundlagen in Mobbing 2",
+        "WiSe 19/20",
+        "Gruppe 3"
+    ),
+    (
+        "Grundlagen in Mobbing 2",
+        "WiSe 19/20",
+        "Gruppe 4"
+    )
