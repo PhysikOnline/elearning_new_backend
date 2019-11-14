bc9d0273bd35f5464191d9db475ae5c76231cc7f
Keiwan Jamaly <keiwanjamaly@gmail.com>
2019-11-14 03:31:02 +0100

    added automatic database updates

--- db/db.js
+++ db/db.js
@@ -23,30 +23,53 @@ pool.getConnection(function(err, connection) {
 
   if (process.env.NODE_ENV === "development") {
     console.log("App is Starting in development");
-  // Execute the database initialisation
+    // Execute the database initialisation
     process.stdout.write("Initializing database");
-  connection.query(fs.readFileSync("db/init.sql").toString(), function(
-    error,
-    results,
-    fields
-  ) {
-    // error hanling
-    if (error) throw error;
+    connection.query(fs.readFileSync("db/init.sql").toString(), function(
+      error,
+      results,
+      fields
+    ) {
+      // error hanling
+      if (error) throw error;
       process.stdout.write(" - done\n");
       process.stdout.write("Filling database with testdata");
-  });
+    });
 
-  // fill the databse with test data
-  connection.query(fs.readFileSync("db/testdata.sql").toString(), function(
-    error,
-    results,
-    fields
-  ) {
-    // error handling
-    if (error) throw error;
+    // fill the databse with test data
+    connection.query(fs.readFileSync("db/testdata.sql").toString(), function(
+      error,
+      results,
+      fields
+    ) {
+      // error handling
+      if (error) throw error;
       process.stdout.write(" - done\n");
-  });
+    });
+    // update the database
+    update(connection);
   }
+  // connection.query("SELECT value FROM Settings WHERE `key`='version'", function(
+  //   error,
+  //   results,
+  //   fields
+  // ) {
+  //   if (error) throw error;
+  //   let version = Number(results[0].value);
+  //   fs.readdir("db/updates/", (err, allUpdates) => {
+  //     if (err) throw err;
+  //     let updates = allUpdates
+  //       .map(x => x.replace(".sql", ""))
+  //       .map(x => Number(x))
+  //       .filter(x => x > version)
+  //       .sort((a, b) => a - b);
+  //     if (updates[updates.length - 1] !== version) {
+  //       throw new Error("Database is not up to date");
+  //     } else {
+  //       console.log("Database is up to date");
+  //     }
+  //   });
+  // });
   // release the connection back to the pool so other functions can use it again
   connection.release();
 });
